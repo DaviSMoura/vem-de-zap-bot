ea6b1bdd20195b63423b9f8540732142cba918d2
Davi Moura <davi.dev@icloud.com>
2021-06-05 22:11:52 -0300

    feat: first release

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 require('dotenv').config()
-const axios = require('axios')
 const { zappp } = require('./zap')
+const Twitter = require('twitter')
+const axios = require('axios')
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+//Bearer headers
+const headers = {
+  'Authorization': 'Bearer ' + process.env.TWITTER_BEARER_TOKEN
+}
 
 async function main() {
-  const { data } = await axios.get('https://api.twitter.com/2/users/2244994945/mentions', {
-    headers: {
-      'Authorization': 'Bearer ' + process.env.TWITTER_BEARER_TOKEN
+  const { data } = await axios.get(`https://api.twitter.com/2/users/${process.env.ACCOUNT_ID}/mentions`, { headers })
+
+  if (data.meta.result_count > 0) {
+    for (const tweet of data.data) {
+      const tweetOrig = await axios.get(`https://api.twitter.com/2/tweets/${tweet.id}?expansions=referenced_tweets.id,author_id`, { headers })
+      const authorInfo = await axios.get(`https://api.twitter.com/2/users/${tweetOrig.data.data.author_id}`, { headers })
+      const authorUsername = authorInfo.data.data.username
+      if (tweetOrig.data.data.referenced_tweets) {
+        const referencedTweets = tweetOrig.data.data.referenced_tweets
+        const lastTweetObj = referencedTweets[referencedTweets.length - 1]
+        const lastTweet = await axios.get(`https://api.twitter.com/2/tweets/${lastTweetObj.id}`, { headers })
+        const lastTweetData = lastTweet.data.data
+        
+        await tweetar({
+          status: '@' + authorUsername + ' ' + zappp(lastTweetData.text),
+          in_reply_to_status_id: tweet.id
+        })
+      } else {
+        await tweetar({
+          status: '@' + authorUsername + ' ' + 'Me mencione respondendo algum tweet para funcionar!',
+          in_reply_to_status_id: tweet.id
+        })
+      }
     }
-  })
-  for (const tweet of data.data) {
-    console.log(zappp(tweet.text) + "\n")
+  } else {
+    console.log('Não tem nenhuma menção.')
   }
 }
 
-main()
+async function tweetar(params) {
+  const replyRequest = await client.post(`statuses/update.json`, params)
+  console.log('Acabei de tweetar algo! (' + replyRequest.text + ')')
+}
+
+console.log('[BOT] Iniciando...')
+main().catch(err => {
+  console.log(err.response ? err.response.data : err || err)
+}).then(() => console.log('[BOT] Finalizado'))
