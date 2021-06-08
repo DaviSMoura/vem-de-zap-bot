871654939b458061897c80818d8ee27e925401a5
Davi Moura <davi.dev@icloud.com>
2021-06-07 22:01:42 -0300

    feat: everything working :). life is beautiful.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { zappp } = require('./zap')
 const Twitter = require('twitter')
-const axios = require('axios')
 const client = new Twitter({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET,
@@ -9,47 +8,63 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-//Bearer headers
-const headers = {
-  'Authorization': 'Bearer ' + process.env.TWITTER_BEARER_TOKEN
+async function main() {
+  client.stream('statuses/filter', { track: process.env.botMention }, function(stream) {
+    stream.on('data', onData)
+    stream.on('error', onError)
+    stream.on('end', onEnd)
+  })
+  return
 }
 
-async function main() {
-  const { data } = await axios.get(`https://api.twitter.com/2/users/${process.env.ACCOUNT_ID}/mentions`, { headers })
-
-  if (data.meta.result_count > 0) {
-    for (const tweet of data.data) {
-      const tweetOrig = await axios.get(`https://api.twitter.com/2/tweets/${tweet.id}?expansions=referenced_tweets.id,author_id`, { headers })
-      const authorInfo = await axios.get(`https://api.twitter.com/2/users/${tweetOrig.data.data.author_id}`, { headers })
-      const authorUsername = authorInfo.data.data.username
-      if (tweetOrig.data.data.referenced_tweets) {
-        const referencedTweets = tweetOrig.data.data.referenced_tweets
-        const lastTweetObj = referencedTweets[referencedTweets.length - 1]
-        const lastTweet = await axios.get(`https://api.twitter.com/2/tweets/${lastTweetObj.id}`, { headers })
-        const lastTweetData = lastTweet.data.data
-        
-        await tweetar({
-          status: '@' + authorUsername + ' ' + zappp(lastTweetData.text),
-          in_reply_to_status_id: tweet.id
-        })
-      } else {
-        await tweetar({
-          status: '@' + authorUsername + ' ' + 'Me mencione respondendo algum tweet para funcionar!',
-          in_reply_to_status_id: tweet.id
-        })
+async function onData(tweet) {
+  if (tweet.in_reply_to_status_id) {
+    await client.get('statuses/lookup', { id: tweet.in_reply_to_status_id_str, tweet_mode: 'extended' }, async (err,tweetOrig) => {
+      if (err) {
+        return console.error(err)
       }
-    }
+
+      const originalText = (tweetOrig[0].full_text || tweetOrig[0].text).replace(new RegExp(/(?<=^|\s)(@[\w.]+)(?<!\.) /gi), '')
+      await tweetar({
+        status: `@${tweet.user.screen_name} ${zappp(originalText)}`.substring(0,279),
+        in_reply_to_status_id: tweet.id_str
+      })
+    })
   } else {
-    console.log('Não tem nenhuma menção.')
+    await tweetar({
+      status: `@${tweet.user.screen_name} Me mencione 😎😘😏😎😻 em resposta 😉👉👌😎😻😋 a um tweet 🙊😎😏👉👌😎 e veja 😼😉 a mágica 😜😋 acontecer 😼😎`,
+      in_reply_to_status_id: tweet.id_str
+    })
   }
 }
 
+function onError(data) {
+  console.error('[BOT] Algo de ruim aconteceu!')
+  console.error(data)
+}
+
+function onEnd(data) {
+  console.log('[BOT] Finalizado! Vamos iniciar novamente já já!')
+  setTimeout(() => {
+    console.log('[BOT] Tentando novamente...')
+    main()
+  }, 5*1000)
+}
+
 async function tweetar(params) {
-  const replyRequest = await client.post(`statuses/update.json`, params)
-  console.log('Acabei de tweetar algo! (' + replyRequest.text + ')')
+  try {
+    const replyRequest = await client.post(`statuses/update.json`, params)
+    console.log('Acabei de tweetar algo! (' + replyRequest.text + ')')
+  } catch (error) {
+    console.error('[ERRO] Erro ao tentar tweetar!')
+    console.error(error)
+  }
 }
 
 console.log('[BOT] Iniciando...')
-main().catch(err => {
-  console.log(err.response ? err.response.data : err || err)
-}).then(() => console.log('[BOT] Finalizado'))
+try {
+  main()
+} catch (err) {
+  console.error('[ERRO] Erro geral', err)
+}
+//.then(() => console.log('[BOT] Finalizado'))
